6b652975d131937d17818a542058c77e2e2f29d2
rajagopal067 <raj.bhojan@gmail.com>
2015-11-17 20:31:57 -0800

    updated code

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,83 +1,77 @@
-# example of program that calculates the average degree of hashtags
 import sys
 import json
 import itertools
 import datetime
 
 class GraphBuilder:
-    #this will store all the edges and vertices of the graph
-    graph = {}
-    curr_tweets = []
 
-    def readTweets(self,inputFileName,outputFileName):
+    graph = {}
+    #this will read the tweets input file and write in (timestamp,hashtags) format)
+    def readTweets(self,inputFileName):
         inputFile = open(inputFileName,'r')
-        outputFile = open(outputFileName,'w')
+        tweets = []
         for line in inputFile:
             lineObj = json.loads(line)
+            if 'created_at' not in lineObj or 'entities' not in lineObj:
+                continue
             tags = lineObj['entities']['hashtags']
             timestamp = lineObj['created_at']
-            dateObj = datetime.datetime.strptime(timestamp, '%a %b %d %H:%M:%S %z %Y')
+            dateObj = datetime.datetime.strptime(timestamp, '%a %b %d %H:%M:%S +0000 %Y')
             hashtags = self.getHashTags(tags)
-            self.addTweet((dateObj,hashtags))
-            avgdegree = self.computeAverageDegree()
-            outputFile.write(str(avgdegree)+"\n")
+            tweets.append((dateObj,hashtags))
+        return tweets
 
-    def addTweet(self,tweet):
-        #tweet is of format tuple(timestamp,list of hashtags)
-        if len(self.curr_tweets) == 0:
-            self.curr_tweets.append(tweet)
-            self.addHashTagsToGraph(tweet)
-            self.computeAverageDegree()
-            return
-        oldest_tweet = self.curr_tweets[0]
-
-        if (tweet[0] - oldest_tweet[0]).seconds < 60:
-            self.curr_tweets.append(tweet)
-            self.addHashTagsToGraph(tweet)
-        else:
-            j=0
-            while j < len(self.curr_tweets) and (tweet[0] - self.curr_tweets[j][0]).seconds > 60:
-                hashtags = self.curr_tweets[j][1]
-                edges = itertools.permutations(hashtags,2)
-                for edge in edges:
-                    self.graph[edge[0]].remove(edge[1])
-                j +=1
-            if j !=0:
-                self.curr_tweets = self.curr_tweets[j:]
-
-    def addHashTagsToGraph(self,tweet):
-        hashtags = tweet[1]
-        edges = itertools.permutations(hashtags,2)
-        for edge in edges:
-            if edge[0] not in self.graph:
-                self.graph[edge[0]] = [edge[1]]
-            else:
-                self.graph[edge[0]].append(edge[1])
-
-
-#this will compute the average degree of all the vertices in the graph.
-    def computeAverageDegree(self):
-        sum = 0.00
-        for key in self.graph.keys():
-            sum += len(set(self.graph[key]))
-
-        if sum == 0:
-            return '0.00'
-        avg_degree = sum / len(self.graph.keys())
-        avg_degree = ( "%.2f" % avg_degree)
-        return avg_degree
-
-
+    #this will get the hashtags from the entities array
     def getHashTags(self,hashtags):
         tags = []
         for tag in hashtags:
-            if len(tag['text']) > 0:
-                tags.append(tag['text'].lower())
+            tag = (tag["text"]).encode("ascii","ignore").lower()
+            if len(tag) > 0:
+                tags.append(tag)
         return list(set(tags))
 
+    #this will create graph from tweets
+    def createGraph(self,tweets,outpuFileName):
+        outputFile = open(outputFileName,'w')
+        i = 0
+        for j in range(len(tweets)):
+            while((tweets[j][0] - tweets[i][0]).seconds > 60):
 
-if __name__ == '__main__':
+                if len(tweets[i][1]) > 1:
+                    edges = itertools.permutations((tweets[i][1]),2)
+                    for edge in edges:
+                        self.graph[edge[0]].remove(edge[1])
+                i+=1
+
+            if len(tweets[j][1]) > 1:
+                edges = itertools.permutations((tweets[j][1]),2)
+                for edge in edges:
+                    if edge[0] in self.graph:
+                        self.graph[edge[0]].append(edge[1])
+                    else:
+                        self.graph[edge[0]] = [edge[1]]
+
+            #Compute the rolling Degree and write to a file
+            outputFile.write(self.computeAverageDegree() + "\n")
+
+
+    def computeAverageDegree(self):
+        degree = 0.0
+        if len(self.graph) ==0:
+            res = format(0.00,'.2f')
+        else:
+            for node in self.graph:
+                degree += len(set((self.graph[node])))
+            avg_degree = degree / len(self.graph.keys())
+            res = ( "%.2f" % avg_degree)
+        return res
+
+
+if __name__=="__main__":
     inputFileName = sys.argv[1]
     outputFileName = sys.argv[2]
-    graphbuilder = GraphBuilder()
-    graphbuilder.readTweets(inputFileName,outputFileName)
+    graphBuilder = GraphBuilder()
+
+    tweets = graphBuilder.readTweets(inputFileName)
+    graphBuilder.createGraph(tweets,outputFileName)
+
