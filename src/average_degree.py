d149cb0587f85a0a1ba2308bfb091b87b2ac290a
rajagopal067 <raj.bhojan@gmail.com>
2015-11-17 16:40:02 -0800

    updated the code

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,1 +1,83 @@
 # example of program that calculates the average degree of hashtags
+import sys
+import json
+import itertools
+import datetime
+
+class GraphBuilder:
+    #this will store all the edges and vertices of the graph
+    graph = {}
+    curr_tweets = []
+
+    def readTweets(self,inputFileName,outputFileName):
+        inputFile = open(inputFileName,'r')
+        outputFile = open(outputFileName,'w')
+        for line in inputFile:
+            lineObj = json.loads(line)
+            tags = lineObj['entities']['hashtags']
+            timestamp = lineObj['created_at']
+            dateObj = datetime.datetime.strptime(timestamp, '%a %b %d %H:%M:%S %z %Y')
+            hashtags = self.getHashTags(tags)
+            self.addTweet((dateObj,hashtags))
+            avgdegree = self.computeAverageDegree()
+            outputFile.write(str(avgdegree)+"\n")
+
+    def addTweet(self,tweet):
+        #tweet is of format tuple(timestamp,list of hashtags)
+        if len(self.curr_tweets) == 0:
+            self.curr_tweets.append(tweet)
+            self.addHashTagsToGraph(tweet)
+            self.computeAverageDegree()
+            return
+        oldest_tweet = self.curr_tweets[0]
+
+        if (tweet[0] - oldest_tweet[0]).seconds < 60:
+            self.curr_tweets.append(tweet)
+            self.addHashTagsToGraph(tweet)
+        else:
+            j=0
+            while j < len(self.curr_tweets) and (tweet[0] - self.curr_tweets[j][0]).seconds > 60:
+                hashtags = self.curr_tweets[j][1]
+                edges = itertools.permutations(hashtags,2)
+                for edge in edges:
+                    self.graph[edge[0]].remove(edge[1])
+                j +=1
+            if j !=0:
+                self.curr_tweets = self.curr_tweets[j:]
+
+    def addHashTagsToGraph(self,tweet):
+        hashtags = tweet[1]
+        edges = itertools.permutations(hashtags,2)
+        for edge in edges:
+            if edge[0] not in self.graph:
+                self.graph[edge[0]] = [edge[1]]
+            else:
+                self.graph[edge[0]].append(edge[1])
+
+
+#this will compute the average degree of all the vertices in the graph.
+    def computeAverageDegree(self):
+        sum = 0.00
+        for key in self.graph.keys():
+            sum += len(set(self.graph[key]))
+
+        if sum == 0:
+            return '0.00'
+        avg_degree = sum / len(self.graph.keys())
+        avg_degree = ( "%.2f" % avg_degree)
+        return avg_degree
+
+
+    def getHashTags(self,hashtags):
+        tags = []
+        for tag in hashtags:
+            if len(tag['text']) > 0:
+                tags.append(tag['text'].lower())
+        return list(set(tags))
+
+
+if __name__ == '__main__':
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+    graphbuilder = GraphBuilder()
+    graphbuilder.readTweets(inputFileName,outputFileName)
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,58 @@
 # example of program that calculates the number of tweets cleaned
+import sys
+import json
+import codecs
+import re
+
+
+class Cleaner:
+
+    count = 0
+
+    def clean_tweets(self,inputFileName,outputFileName):
+        inputFile = open(inputFileName,'r')
+        outputFile = open(outputFileName,'w')
+        for line in inputFile:
+            lineObj = json.loads(line)
+            if 'created_at' in lineObj and 'text' in lineObj:
+                timestamp = lineObj['created_at']
+                text = lineObj['text']
+                cleaned_tweet = self.get_cleantext(text)
+                outputFile.write(cleaned_tweet + " (timestamp: "+timestamp+ ")\n")
+
+        outputFile.write("\n" + str(self.count) + " tweets contain unicode")
+
+
+    def isAscii(self,c):
+        try:
+            return ord(c) <= 127
+        except:
+            return False
+
+    def get_cleantext(self,text):
+
+        containsUnicode = False
+        #remove \n and \t replace with whitespace
+        text = text.replace('\n',' ').replace('\t',' ')
+        clean_text = ""
+        for c in text:
+            if self.isAscii(c):
+                clean_text += str(c)
+            else:
+                containsUnicode = True
+
+        if containsUnicode == True:
+            self.count += 1
+
+        return clean_text
+
+
+
+
+
+if __name__ == '__main__':
+    inputFilename = sys.argv[1]
+    outputFilename = sys.argv[2]
+
+    cleaner = Cleaner()
+    cleaner.clean_tweets(inputFilename,outputFilename)
